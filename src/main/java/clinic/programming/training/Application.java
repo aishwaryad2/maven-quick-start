44a48a310123f77844ac9f7b0b455c28e5fd4e76
Deshpande, Aishwarya <deshpandeaishwarya16@gmail.com>
2020-05-17 17:02:28 +0530

    Performing changes

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -9,6 +9,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
+		System.out.println ("Hello world, welcome to my Application");
 		Application app = new Application();
     }
 }
